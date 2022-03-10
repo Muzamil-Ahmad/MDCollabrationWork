42064ed1c5969c7a20320084ff32c31cccd0cd5f
unknown <mareshi@appstec-me.com>
2022-03-10 22:36:11 +0530

    changes

--- server.js
+++ server.js
@@ -4,18 +4,28 @@ const app = express();
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+// const routes = require("./routes/tutorial.routes.js")(app);
+debugger
+
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}));
 // simple route
+const db = require("./models");
+db.sequelize.sync({force: true}).then(() => {
+  console.log("Drop and re-sync db.");
+});
 app.get("/", (req, res) => {
+  debugger
   res.json({message: "Welcome to your first project."});
 });
-app.get("/index", (req, res) => {
-  res.json({message: "Welcome to zameer, muzamil and danish."});
-});
+// app.get("/index", (req, res) => {
+//   res.json({message: "Welcome to zameer, muzamil and danish."});
+// });
+require("./routes/tutorial.routes.js")(app);
+// app.use('/', routes);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
